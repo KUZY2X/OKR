bb6a9a609af1e5d99ee8f8aed948579e3ba0283b
vas96 <antal_lviv@ramber.ru>
2017-03-29 23:46:31 +0300

    like

---
+++ TB/TeamBuilding/Tabs/ProfileTab.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TeamBuilding.Tabs
+{
+    public partial class ProfileTab : UserControl
+    {
+        public TeamBuildingEntities1 TeamBuildingEntities = new TeamBuildingEntities1();
+        public ObservableCollection<Users> UsersList;
+
+        public ProfileTab()
+        {
+            InitializeComponent();
+        }
+
+        public bool LoadUserData(int selected)
+        {
+            try
+            {
+                UsersList = new ObservableCollection<Users>(TeamBuildingEntities.Users);
+
+                var chosenOne = UsersList[selected];
+                pictureBox1.Image = new Bitmap(@"..\..\Pictures\" + chosenOne.PicturePath);
+                bunifuCustomLabel2.Text = chosenOne.Name;
+                bunifuCustomLabel3.Text = "Joined: " + chosenOne.Registered;
+                bunifuCustomLabel4.Text = "Your projects: " + chosenOne.Projects1.Count;
+
+                try
+                {
+                    var userContacts = UsersList[selected].Contacts;
+                    {
+                        if (userContacts.PublicMail != null)
+                            bunifuCustomLabel5.Text = "Contacts: Email: " + userContacts.PublicMail + "; ";
+                        if (userContacts.Facebook != null)
+                            bunifuCustomLabel5.Text += "Facebook: " + userContacts.Facebook + "; ";
+                        if (userContacts.VKId != null)
+                            bunifuCustomLabel5.Text += "VK: " + userContacts.VKId + "; ";
+                        if (userContacts.Linkedin != null)
+                            bunifuCustomLabel5.Text += "LinkedIn: " + userContacts.Linkedin + "; ";
+                    }
+                }
+
+                catch (Exception)
+                {
+                    bunifuCustomLabel5.Text = "Contacts: none";
+                }
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
+
+            return true;
+        }
+
+        private void bunifuThinButton23_Click(object sender, EventArgs e)
+        {
+            ProjectTab.Instance.Visible = true;
+            ProjectTab.Instance.StartInfo();
+            Controls.Add(ProjectTab.Instance);
+            ProjectTab.Instance.Dock = DockStyle.Fill;
+            ProjectTab.Instance.BringToFront();
+        }
+    }
+}
